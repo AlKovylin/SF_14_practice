0f152de8e5d88645c3d6da2d292c32bcf5172055
Ковылин Алексей <alkovylin@ya.ru>
2021-12-21 16:51:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace SF_14_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneBookReader PR = new PhoneBookReader();
+
+            try
+            {
+                PR.CreatPhoneBook();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Ошибка открытия файла: " + e.Message);
+            }
+            //PR.CreatPhoneBook_();
+            //PR.ReadBook();
+        }
+    }
+}
